d2b7518783311cd80aaafc0b7aaef4d02970ebbf
Giacomo Zarba Meli <giacomo.zarba@immobiliare.it>
2023-10-27 17:36:31 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Usa un'immagine Node.js come base
+FROM node:16
+
+# Crea una directory di lavoro nel container
+WORKDIR /
+
+# Copia i file del tuo progetto nel container
+COPY package*.json ./
+COPY server.js ./
+
+# Installa le dipendenze del progetto
+RUN npm install
+
+# Esponi la porta su cui il server Fastify ascolterà
+EXPOSE 3002
+
+# Comando per avviare il server Fastify
+CMD ["node", "server.js"]
---
+++ server.js
@@ -0,0 +1,61 @@
+const fastify = require('fastify')({ logger: { level: 'debug' } });
+const axios = require('axios');
+const cors = require('@fastify/cors')
+
+const pythonBackend = 'http://backend-python:5000'
+const goBackend = 'http://backend-go:5001'
+
+fastify.register(cors, {
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization'
+});
+
+fastify.post('/login', async (request, reply) => {
+  try {
+    const responseGo = await axios.post(`${goBackend}/login`, request.body);
+
+    // In questo caso, solo il login è gestito dal backend Go
+    reply.send(responseGo.data);
+  } catch (error) {
+    reply.code(500).send({ error: 'Internal Server Error' });
+  }
+});
+
+fastify.post('/post_name', async (request, reply) => {
+  try {
+    // Inoltra la richiesta al backend Python
+    const response = await axios.post(`${pythonBackend}/post_name`, request.body);
+    reply.send(response.data);
+  } catch (error) {
+    reply.code(500).send({ error: 'Internal Server Error' });
+  }
+});
+
+fastify.post('/book', async (request, reply) => {
+  try {
+    // Inoltra la richiesta al backend Python per la route "/book"
+    const response = await axios.post(`${pythonBackend}/book`, request.body);
+    reply.send(response.data);
+  } catch (error) {
+    reply.code(500).send({ error: 'Internal Server Error' });
+  }
+});
+
+fastify.post('/myreservations', async (request, reply) => {
+  try {
+    // Inoltra la richiesta al backend Python per la route "/myreservations"
+    const response = await axios.post(`${pythonBackend}/myreservations`, request.body);
+    reply.send(response.data);
+  } catch (error) {
+    reply.code(500).send({ error: 'Internal Server Error' });
+  }
+});
+
+fastify.listen(3002, '0.0.0.0', (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log('BFF Server is running on port 3002');
+});
